71acfc03a8ae7fa03425bf05791fb572349af046
cobriensr <charles.a.obrien@outlook.com>
2024-11-28 23:24:51 -0600

    Added parameter store in place of redis

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,6 @@
 # iam.tf
+
+# Create an IAM role for the Lambda function
 resource "aws_iam_role" "lambda_role" {
   name = "${local.name_prefix}-lambda-role"
 
@@ -18,6 +20,7 @@ resource "aws_iam_role" "lambda_role" {
   tags = local.common_tags
 }
 
+# Allow the Lambda function to write logs to CloudWatch, create and delete network interfaces, and retrieve secrets from Secrets Manager
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "${local.name_prefix}-lambda-policy"
   role = aws_iam_role.lambda_role.id
@@ -42,7 +45,28 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
+# Attach the AWSXRayDaemonWriteAccess policy to the Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_xray" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
+
+# Allow the Lambda function to read and write parameters in Parameter Store
+resource "aws_iam_role_policy" "lambda_parameter_store" {
+  name = "parameter_store_access"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:PutParameter"
+        ]
+        Resource = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/tradovate/*"
+      }
+    ]
+  })
 }
