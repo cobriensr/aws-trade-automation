b420ec0df9fc562b20c2d102debe6edb7c415089
cobriensr <charles.a.obrien@outlook.com>
2024-11-29 14:23:36 -0600

    updated lambda iam permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -37,7 +37,8 @@ resource "aws_iam_role_policy" "lambda_policy" {
           "ec2:CreateNetworkInterface",
           "ec2:DescribeNetworkInterfaces",
           "ec2:DeleteNetworkInterface",
-          "secretsmanager:GetSecretValue"
+          "secretsmanager:GetSecretValue",
+          "lambda:InvokeFunction"
         ]
         Resource = "*"
       }
