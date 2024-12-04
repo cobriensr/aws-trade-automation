70f6f9bcf1dd62a2965acb383e65217f7658f799
cobriensr <charles.a.obrien@outlook.com>
2024-12-04 00:23:08 -0600

    updated lambda layer for numpy issue

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "main" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "main.lambda_handler"
   runtime       = "python3.12"
-  layers        = ["arn:aws:lambda:us-east-1:565625954376:layer:databento-layer:7"]
+  layers        = ["arn:aws:lambda:us-east-1:565625954376:layer:databento-layer:8"]
   timeout       = 30
   memory_size   = 1024
 
