ff3c658febf55d973348617fb5c01d31c11bdaa7
cobriensr <charles.a.obrien@outlook.com>
2024-12-06 14:40:36 -0600

    reduced database complexity to save cost

--- terraform/database.tf
+++ terraform/database.tf
@@ -4,8 +4,8 @@ resource "aws_rds_cluster" "trading_db" {
   engine_version                  = "16.4"
   database_name                   = "tradeAutomation"
   master_username                 = "postgres"
-  deletion_protection             = true
-  backup_retention_period         = 7
+  deletion_protection             = false
+  backup_retention_period         = 2
   preferred_backup_window         = "05:24-05:54"
   preferred_maintenance_window    = "sun:04:25-sun:04:55"
   db_subnet_group_name            = "default-vpc-027bf578d09033c51"
@@ -13,27 +13,20 @@ resource "aws_rds_cluster" "trading_db" {
   storage_encrypted               = true
   enabled_cloudwatch_logs_exports = ["postgresql"]
   copy_tags_to_snapshot           = true
-  enable_http_endpoint            = true
-  skip_final_snapshot             = true
-  enable_global_write_forwarding  = true
-  enable_local_write_forwarding   = true
+  enable_http_endpoint            = false
+  skip_final_snapshot            = true
   serverlessv2_scaling_configuration {
-    min_capacity = 8
-    max_capacity = 64
+    min_capacity = 0.5
+    max_capacity = 2
   }
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-
+  performance_insights_enabled    = false
 }
 
 resource "aws_rds_cluster_instance" "trading_db_instances" {
-  count               = 2
-  identifier          = count.index == 0 ? "trade-automation-instance-1" : "trade-automation-instance-1-us-east-1b"
-  cluster_identifier  = aws_rds_cluster.trading_db.id
-  instance_class      = "db.serverless"
-  engine              = aws_rds_cluster.trading_db.engine
-  engine_version      = aws_rds_cluster.trading_db.engine_version
-  promotion_tier      = 1
-  monitoring_interval = 60
-  monitoring_role_arn = "arn:aws:iam::565625954376:role/rds-monitoring-role"
+  count              = 1
+  identifier         = "trade-automation-instance-1"
+  cluster_identifier = aws_rds_cluster.trading_db.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.trading_db.engine
+  engine_version     = aws_rds_cluster.trading_db.engine_version
 }
