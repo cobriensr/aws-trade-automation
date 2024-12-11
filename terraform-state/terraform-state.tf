9d6ae3029f6f569b2ec255a59b2cc51f5f425612
cobriensr <charles.a.obrien@outlook.com>
2024-12-11 16:29:31 -0600

    security update [no ci]

--- terraform-state/terraform-state.tf
+++ terraform-state/terraform-state.tf
@@ -37,3 +37,45 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# S3 bucket for storing access logs of the Terraform state bucket
+resource "aws_s3_bucket" "terraform_state_access_logs" {
+  bucket = "trading-terraform-state-access-logs"
+}
+
+# Enable versioning for the access logs bucket
+resource "aws_s3_bucket_versioning" "terraform_state_access_logs" {
+  bucket = aws_s3_bucket.terraform_state_access_logs.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable encryption for the access logs bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_access_logs" {
+  bucket = aws_s3_bucket.terraform_state_access_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block public access for the access logs bucket
+resource "aws_s3_bucket_public_access_block" "terraform_state_access_logs" {
+  bucket = aws_s3_bucket.terraform_state_access_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable logging for the Terraform state bucket
+resource "aws_s3_bucket_logging" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  target_bucket = "trading-terraform-state-access-logs"
+  target_prefix = "terraform-state-logs/"
+}
